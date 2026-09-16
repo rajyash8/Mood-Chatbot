cad91712372a32d14b51660bafd8a6ac1d61578d
Yash <163318000+rajyash8@users.noreply.github.com>
2026-09-16 18:41:26 +0530

    Update chatbot.py

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -1,23 +1,24 @@
+import streamlit as st
+
 from langchain_huggingface import (
     ChatHuggingFace,
     HuggingFacePipeline
 )
 
 
+@st.cache_resource
 def get_chat_model():
 
     llm = HuggingFacePipeline.from_model_id(
         model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
         task="text-generation",
         pipeline_kwargs={
-            "max_new_tokens": 256,
+            "max_new_tokens": 128,
             "do_sample": False,
             "repetition_penalty": 1.03,
         },
     )
 
-    chat_model = ChatHuggingFace(
+    return ChatHuggingFace(
         llm=llm
     )
-
-    return chat_model
